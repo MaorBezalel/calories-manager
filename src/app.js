fe047bca79f1c29f8fae7d83777ef0f668d710f7
Maor <maorbz858@gmail.com>
2024-02-20 16:01:11 +0200

    Add view engine setup to app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
+// simple server to test the app (write in the terminal: `npm run start:dev`)
+
 import express from 'express';
 
 const app = express();
-
-// simple server to test the app (write in the terminal: `npm run start:dev`)
-
 const PORT = process.env.PORT || 3000;
+
+// view engine setup
+app.set('views', './views');
+app.set('view engine', 'pug');
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);

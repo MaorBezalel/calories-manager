1188aa35c4db91af38dea052f415555cd72f7069
Maor <maorbz858@gmail.com>
2024-02-27 12:17:17 +0200

    Add dotenv package and connect to MongoDB via Mongoose

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
 /**
  * @fileoverview This file contains the main app that runs the server.
  *
- * @author Maor Bezalel 325214443
+ * @author Maor Bezalel
  * @author <firstname> <lastname> <id> @todo add your info Itzik (delete the todo after adding the info)
  */
 
-// import express
+// import express for creating the server
 import express from 'express';
 
-// import routes
+// import mongoose for database connection
+import mongoose from 'mongoose';
+
+// import dotenv for environment variables
+import 'dotenv/config';
+
+// import routes (endpoints)
 import indexRouter from './routes/index.js';
 import aboutRouter from './routes/about.js';
 import addcaloriesRouter from './routes/addcalories.js';
@@ -17,11 +23,20 @@ import reportRouter from './routes/report.js';
 // create an express app
 const app = express();
 
+// connect to the database using mongoose
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log('MongoDB Connected...'))
+    .catch((err) => console.log(err));
+
 // use routes
 app.use(indexRouter);
 app.use(aboutRouter);
 app.use(addcaloriesRouter);
 app.use(reportRouter);
+
+// use express middlewares
+app.use(express.json()); // for parsing application/json
 
 // view engine setup
 app.set('view engine', 'pug');

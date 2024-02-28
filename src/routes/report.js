d9b75ff07ac06e93acef3427968e26668b9f79e1
Maor <maorbz858@gmail.com>
2024-02-28 20:53:23 +0200

    Implement `/report/` route with validation schemes and additional middleware

--- src/routes/report.js
+++ src/routes/report.js
@@ -6,40 +6,50 @@
  */
 
 import { Router } from 'express';
+import { checkSchema } from 'express-validator';
+import {
+    handleValidationErrorsMiddleware,
+    checkIfUserExistsMiddleware,
+    fetchAndGenaerateReportMiddleware,
+} from '../middlewares/index.js';
+import { getReportOnCalorieConsumptionValidationScheme } from '../validation-schemas/index.js';
 
 const router = Router();
 
-router.get('/report', (req, res) => {
-    /**
-     * @todo Add the necessary code to handle the GET endpoint /report (instructions are below).
-     * @todo Create/Use pug files to render the report upon successful retrieval of the data from the MongoDB database or an error view if the data is not retrieved. It main serves as a feedback to users that make requests to the server directly from the browser.
-     *
-     * @instructions
-     *
-     *    |
-     *    |
-     *    V
-     *
-     * @method GET
-     * @description This endpoint is used to retrieve a detailed report of calorie consumption for a specific month and year.
-     *
-     * QUERY PARAMETERS received in the request:
-     * @param user_id - The ID of the user for whom the report is being requested.
-     * @param year - The year for which the report is being requested.
-     * @param month - The month for which the report is being requested.
-     *
-     * @functionality The endpoint should query the MongoDB database for calorie consumption items matching the specified user, year, and month. It should then generate a JSON response containing a detailed report of calorie consumption for different categories (breakfast, lunch, dinner, other) for the specified month and year.
-     *
-     * @example The JSON response should look like this:
-     * ```json
-     * {
-     *   "breakfast": [{ "day": 21, "description": "chocolate in ikea", "amount": 300 }, { "day": 5, "description": "milk", "amount": 6 }],
-     *   "lunch": [],
-     *   "dinner": [],
-     *   "other": []
-     * }
-     * ```
-     */
-});
+/**
+ * @api {get} /report Report
+ * @apiName Report
+ * @apiGroup Calories
+ * @apiDescription This endpoint is used to retrieve a detailed report of calorie consumption for a specific user in a specific month and year.
+ *
+ * @apiQuery {Number} user_id The ID of the user for whom the report is being requested.
+ * @apiQuery {Number} year The year in which calories were consumed.
+ * @apiQuery {Number} month The month in which calories were consumed.
+ *
+ * @apiSuccess (200) {Object} Report A detailed JSON report of calorie consumption for different categories (breakfast, lunch, dinner, other) for the specified month and year.
+ * @apiUse ReportExample
+ *
+ * @apiError (400) {Object} ValidationErrors An array of errors that occurred during the validation of the request parameters (body, query, or path params).
+ * @apiUse ErrorValidationExample
+ * @apiError (404) {String} UserNotFound User with the provided {user_id} does not exist; therefore, a report cannot be generated.
+ * @apiError (500) {String} InternalServerError The server encountered an internal error while trying to generate the report for the user.
+ */
+router.get(
+    '/report',
+    checkSchema(getReportOnCalorieConsumptionValidationScheme),
+    handleValidationErrorsMiddleware,
+    checkIfUserExistsMiddleware(true),
+    fetchAndGenaerateReportMiddleware,
+    async (req, res) => {
+        // get the validated data from the locals object of the response object (attached by the `handleValidationErrorsMiddleware`)
+        const data = res.locals.validatedData;
+
+        // get the report from the locals object of the response object (attached by the `fetchAndGenaerateReportMiddleware`)
+        const report = res.locals.reportData;
+
+        // return to the client the report as a JSON response
+        res.status(200).json(report);
+    }
+);
 
 export default router;

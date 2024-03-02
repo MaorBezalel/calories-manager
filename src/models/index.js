8ee5cbf89e9b2b39afead6f1c88b17a756a26a19
Maor <maorbz858@gmail.com>
2024-03-02 19:38:49 +0200

    Implement `/about/` route

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,5 @@
 import { User } from './User.js';
 import { CalorieConsumption } from './CalorieConsumption.js';
+import { Developer } from './Developer.js';
 
-export { User, CalorieConsumption };
+export { User, CalorieConsumption, Developer };
--- src/routes/about.js
+++ src/routes/about.js
@@ -2,40 +2,36 @@
  * @fileoverview This file contains the route for the endpoint /about.
  *
  * @author Maor Bezalel
- * @author @todo add your info Itzik (delete the todo after adding the info)
+ * @author Itzhak Yakubov
  */
 
 import { Router } from 'express';
+import { Developer } from '../models/index.js';
 
 const router = Router();
 
-router.get('/about', (req, res) => {
-    /**
-     * @todo Add the necessary code to handle the GET endpoint /about (instructions are below).
-     * @todo Create/Use pug files to render the about view. It main serves as a feedback to users that make requests to the server directly from the browser.
-     *
-     * @instructions
-     *
-     *    |
-     *    |
-     *    V
-     *
-     * @method GET
-     * @description This endpoint is used to retrieve information about the developers.
-     *
-     * @params No parameters expected.
-     *
-     * @functionality The endpoint should return a JSON array containing objects describing each developer involved in the project.
-     * Each object should include details such as first name, last name, ID, and email address of the developers.
-     *
-     * @example The JSON response should look like this:
-     * ```json
-     * [
-     *   {“firstname”:”dave”,”lastname”:”cohen”,”id”:234234,”email”:”daddd@gmail.com”},
-     *   {“firstname”:”tal”,”lastname”:”levy”,”id”:34534544,”email”:”tal@gmail.com”}
-     * ]
-     * ```
-     */
+/**
+ * @api {get} /about About
+ * @apiName About
+ * @apiGroup General
+ * @apiDescription This endpoint is used to retrieve information about the developers.
+ *
+ * @apiSuccess (200) {Object[]} Developers An array of objects describing each developer involved in the project.
+ *
+ * @apiError (404) {String} NotFound No developers found.
+ * @apiError (500) {String} InternalServerError The server encountered an internal error while trying to get the developers.
+ */
+router.get('/about', async (req, res) => {
+    try {
+        const developers = await Developer.find({}, { _id: 0, __v: 0 });
+        if (!developers) {
+            res.status(404).json({ message: 'No developers found' });
+        } else {
+            res.status(200).json(developers);
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 });
 
 export default router;

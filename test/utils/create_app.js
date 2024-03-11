e64e32bf4e3cabfd0428bcea8d63a0d25afab834
Maor <maorbz858@gmail.com>
2024-03-11 21:01:39 +0200

    Add createApp utility function for testing purposes

---
+++ test/utils/create_app.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import { homeRouter, aboutRouter, addcaloriesRouter, reportRouter } from '../../src/routes/index.js';
+
+/**
+ * Create an express app with all the routes.
+ *
+ * @returns {express.Express} the app
+ *
+ * @remarks Used for testing the app without starting the server and connecting to the database.
+ */
+export const createApp = () => {
+    const app = express();
+
+    // use middlewares
+    app.use(express.json()); // for parsing application/json
+
+    // use routes
+    app.use(homeRouter);
+    app.use(aboutRouter);
+    app.use(addcaloriesRouter);
+    app.use(reportRouter);
+    app.use('/docs', express.static('docs')); // serve the docs folder as static files
+
+    // view engine setup
+    app.set('view engine', 'pug');
+    app.set('views', './src/views');
+
+    return app;
+};
